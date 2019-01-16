d3c3d4976e465f33502aeb0b3ba9f4ab05b3a4f6
borilio <exprimetuandroid@gmail.com>
2019-01-16 13:20:26 +0100

    Añadidos comentarios y simplificado un poco el código quitando librerías que no se usan

--- js/codigo.js
+++ js/codigo.js
@@ -4,11 +4,14 @@ Autor: Bori
 Fecha: 16/01/2019
 */
 
+
+//Función que se ejecuta al cargar la página web y cambia el href del enlace
 function inicio() {
     document.querySelector("#btnW").href = "whatsapp://send?text=" + document.URL;
 }
 
-
+//Añadimos un listener al documento de forma que cuando termine de cargar
+//la página, se inicie la función inicio()
 if ("addEventListener" in document) {
     document.addEventListener("DOMContentLoaded", inicio);
 }
